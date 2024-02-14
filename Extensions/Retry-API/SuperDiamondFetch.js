1a085efe1fca89567cda916d25f4c4ec04b2d862
Alex <142623358+Alexidians@users.noreply.github.com>
2024-02-14 13:51:03 +0200

    Create SuperDiamondFetch.js

---
+++ Extensions/Retry-API/SuperDiamondFetch.js
@@ -0,0 +1,6 @@
+function AddScript(url) {
+ var elem = document.createElement("script")
+ elem.src = url
+ document.body.appendChild(elem)
+}
+AddScript("https://alexidians.github.io/Super-Diamond-Fetch/Extensions/Retry-API/AutoRetry.js");
